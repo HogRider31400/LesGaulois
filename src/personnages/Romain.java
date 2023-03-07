550951410d16618fd6d208fdbfb2a4d24ceb389e
Alexandre Ducros <dcl4361a@ens.fsi>
2023-03-07 16:42:46 +0100

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,17 +3,52 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		assert force > 0;
 		this.force = force;
+		equipements = new Equipement[2];
+		
 	}
 	
 	public String getNom() {
 		return nom;
 	}
 	
+	public void sEquiper(Equipement equipement) {
+		switch(nbEquipement) {
+		case 2:
+		{
+			System.out.println("Le solat " + nom + " est déjà bien protégé");
+			break;
+		}
+		case 1:
+		{
+			if(equipements[0].toString() == equipement.toString()) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement.toString());
+			}
+			else {
+				equipements[1] = equipement;
+				nbEquipement++;
+				System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.toString());
+		
+			}
+			break;
+		}
+		default:
+		{
+			nbEquipement++;
+			equipements[0] = equipement;
+			
+			System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.toString());
+			break;
+		}
+		
+	}
+	}
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "� " + texte + "�");
 	}
@@ -35,5 +70,12 @@ public class Romain {
 		System.out.println(ro.prendreParole());
 		ro.parler("salut");
 		ro.recevoirCoup(10000);
+		System.out.println(Equipement.CASQUE + " " + Equipement.BOUCLIER);
+		
+		ro.sEquiper(Equipement.CASQUE);
+		ro.sEquiper(Equipement.CASQUE);
+		ro.sEquiper(Equipement.BOUCLIER);
+		ro.sEquiper(Equipement.CASQUE);
+		
 	}
 }
